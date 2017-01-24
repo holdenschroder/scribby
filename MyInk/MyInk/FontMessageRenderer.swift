51fecb3e23cede933a09bb1c5ff326be09322767
John Lawrence <johnonrails@gmail.com>
2017-01-24 11:19:51 -0800

    Allow for adjustment of margins in renderer

--- MyInk/MyInk/FontMessageRenderer.swift
+++ MyInk/MyInk/FontMessageRenderer.swift
@@ -12,11 +12,19 @@ class FontMessageRenderer
     }
 
     func render(message: String, width: CGFloat, lineHeight: CGFloat, backgroundColor: UIColor, maxAspectRatio: CGFloat = CGFloat.greatestFiniteMagnitude) -> UIImage? {
+    var margin = UIOffset(horizontal: 25, vertical: 25)
+    var alignment: TypeSetterAlignment = .left
+
         let typeSetter = TypeSetter(message: message, lineHeight: lineHeight, width: width, kerning: 0.1, atlases: [_atlas, _fallbackAtlas], maxAspectRatio: maxAspectRatio)
-        typeSetter.margin = UIOffset(horizontal: 25, vertical: 25)
-        typeSetter.set()
+        typeSetter.margin = margin
+        typeSetter.set(alignment: alignment, centerSmallMessages: true)
 
         let size = typeSetter.size
+
+        guard size.width != 0 && !typeSetter.isEmpty else {
+            return nil
+        }
+
         UIGraphicsBeginImageContext(size)
         let graphicsContext = UIGraphicsGetCurrentContext()
         backgroundColor.setFill()
